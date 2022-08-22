05f7b37a6ff522862a053ee31b5149dd6bf582e0
vikaberezuk <vikaberezuk12345@gmail.com>
2022-08-22 10:17:07 +0300

    data

---
+++ src/components/Portfolio/Slider/data.js
@@ -0,0 +1,48 @@
+const people = [
+    {
+        id: 1,
+        image:
+            'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+        name: 'maria ferguson',
+        title: 'Travel Guru',
+        quote:
+            'Lack of communication is the main predicament here. When you contact the call centre in Brighton, people ' +
+            'promise to call you back. I do not feel valued as a cus...'  },
+    {
+        id: 2,
+        image:
+            'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+        name: 'Rhian Griffiths',
+        title: 'regular guy',
+        quote:
+            'Just had Jenny L in Operations from On The Beach deal with an issue we had with our hotel and she has ' +
+            'worked wonders and sorted us all out. What a pleasure to d...'
+    },
+    {
+        id: 3,
+        image:
+            'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+        name: 'Adrian Dobson',
+        title: 'Delivery UK',
+        quote:
+            'Rhenus was the appointed delivery company by JYSK for some outdoor furniture. Whilst the initial contact' +
+            ' email was a little confusing as it had a delivery date ...' },
+    {
+        id: 4,
+        image:
+            'https://images.unsplash.com/photo-1544717305-2782549b5136?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTl8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+        name: 'susan andersen',
+        title: 'the boss',
+        quote:
+            'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+    },
+    {
+        id: 5,
+        image: 'https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+        name: 'James Lewis',
+        title: 'Fitness Superstore',
+        quote: 'Very quick service, perfect communication, delivered by a very pleasant person next day.'
+    }
+];
+
+export default people;
