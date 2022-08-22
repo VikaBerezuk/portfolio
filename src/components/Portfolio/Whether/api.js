830520dfd915ba48dc5d78f5b8971b85def54797
vikaberezuk <vikaberezuk12345@gmail.com>
2022-08-22 10:17:23 +0300

    add redux toolkit

---
+++ src/components/Portfolio/Whether/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "1c8d8317afmsh412897d7d01a107p17b8bajsnf5d5a56be277",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d44fe64ff2e12485694a45893621b601";
